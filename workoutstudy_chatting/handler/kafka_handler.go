9f577df3cf9f055a29f6bc8c403034b56e3a9152
sinsiwayshkim <shkim@sinsiway.com>
2024-04-16 16:21:05 +0900

    FEAT : kafka conumser 8

--- workoutstudy_chatting/handler/kafka_handler.go
+++ workoutstudy_chatting/handler/kafka_handler.go
@@ -2,22 +2,56 @@ package handler
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/segmentio/kafka-go"
 )
 
 // FitMateHandler는 'fit-mate' 토픽의 메시지를 처리합니다.
 func FitMateHandler(msg kafka.Message) {
-	// 'fit-mate' 토픽 메시지에 대한 처리 로직
-	fmt.Printf("Processing 'fit-mate' message: %s\n", string(msg.Value))
-	// 여기에 필요한 비즈니스 로직 추가
+	value, err := strconv.Atoi(string(msg.Value))
+	if err != nil {
+		log.Printf("Error converting Kafka message to int: %v\n", err)
+		return
+	}
+	url := fmt.Sprintf("http://fit-group:8080/fit-group-service/groups/%d", value)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Printf("Error sending GET request: %v\n", err)
+		return
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Error reading response body: %v\n", err)
+		return
+	}
+	fmt.Printf("Response from fit-group-service for groups: %s\n", string(body))
 }
 
 // FitGroupHandler는 'fit-group' 토픽의 메시지를 처리합니다.
 func FitGroupHandler(msg kafka.Message) {
-	// 'fit-group' 토픽 메시지에 대한 처리 로직
-	fmt.Printf("Processing 'fit-group' message: %s\n", string(msg.Value))
-	// 여기에 필요한 비즈니스 로직 추가
+	value, err := strconv.Atoi(string(msg.Value))
+	if err != nil {
+		log.Printf("Error converting Kafka message to int: %v\n", err)
+		return
+	}
+	url := fmt.Sprintf("http://fit-group:8080/fit-group-service/mates/%d", value)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Printf("Error sending GET request: %v\n", err)
+		return
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Error reading response body: %v\n", err)
+		return
+	}
+	fmt.Printf("Response from fit-group-service for mates: %s\n", string(body))
 }
 
 // HandleMessage는 토픽에 따라 적절한 핸들러를 호출합니다.
