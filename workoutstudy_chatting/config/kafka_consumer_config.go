7ba0978c52dccd6826415d3cd26f1d72ce6322cb
Kimsunfang <kimsunfang@gmail.com>
2024-06-23 15:33:55 +0900

    FIX : disable kafka connectin check

--- workoutstudy_chatting/config/kafka_consumer_config.go
+++ workoutstudy_chatting/config/kafka_consumer_config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"context"
 	"log"
-	"time"
 
 	"workoutstudy_chatting/handler"
 
@@ -29,25 +28,25 @@ func NewKafkaConsumer(bootstrapServers string, groupID string, topics []string) 
 		log.Printf("Kafka Reader created for topic: %s", topic) // 토픽별 Kafka Reader 생성 로그 추가
 
 		// Kafka 브로커 연결 확인
-		if err := checkKafkaConnection(reader); err != nil {
-			log.Fatalf("Failed to connect to Kafka broker at %s: %v", bootstrapServers, err)
-		}
-		log.Printf("Successfully connected to Kafka broker at %s for topic %s", bootstrapServers, topic)
+		// if err := checkKafkaConnection(reader); err != nil {
+		// 	log.Fatalf("Failed to connect to Kafka broker at %s: %v", bootstrapServers, err)
+		// }
+		// log.Printf("Successfully connected to Kafka broker at %s for topic %s", bootstrapServers, topic)
 	}
 	return &KafkaConsumer{
 		Readers: readers,
 	}
 }
 
-// Kafka 브로커 연결 확인 함수
-func checkKafkaConnection(reader *kafka.Reader) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+// // Kafka 브로커 연결 확인 함수
+// func checkKafkaConnection(reader *kafka.Reader) error {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel()
 
-	// 메시지 읽기 시도
-	_, err := reader.FetchMessage(ctx)
-	return err
-}
+// 	// 메시지 읽기 시도
+// 	_, err := reader.FetchMessage(ctx)
+// 	return err
+// }
 
 // 메시지 Consume 메서드
 func (kc *KafkaConsumer) Consume(ctx context.Context, msgChan chan handler.MessageEvent) {
