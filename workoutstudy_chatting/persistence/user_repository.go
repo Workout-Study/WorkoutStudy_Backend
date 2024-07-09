a50b050a4ee26c0537fd915b2f665ad0fc28c0f2
Kimsunfang <kimsunfang@gmail.com>
2024-07-09 16:00:15 +0900

    FIX : user-create-event consume service

--- workoutstudy_chatting/persistence/user_repository.go
+++ workoutstudy_chatting/persistence/user_repository.go
@@ -27,7 +27,7 @@ func NewUserRepository(db *sql.DB) UserRepository {
 func (repo *UserRepositoryImpl) SaveUser(user *model.User) (*model.User, error) {
 	query := `INSERT INTO user (id, nickname, state, created_at, created_by, updated_at, updated_by) VALUES ($1, $2, $3, NOW(), $4, NOW(), $5) RETURNING id`
 
-	err := repo.DB.QueryRow(query, user.ID, user.Nickname, user.State, user.CreatedBy, user.UpdatedBy).Scan(&user.ID)
+	err := repo.DB.QueryRow(query, user.ID, user.Nickname, user.State, user.CreatedAt, user.Nickname, user.UpdatedAt, user.Nickname).Scan(&user.ID)
 	if err != nil {
 		log.Printf("Error saving user: %v", err)
 		return nil, fmt.Errorf("error saving user: %w", err)
@@ -39,7 +39,7 @@ func (repo *UserRepositoryImpl) UpdateUser(user *model.User) (*model.User, error
 	query := `UPDATE user SET nickname = $2, state = $3, updated_at = $4, updated_by = $5 WHERE id = $1 RETURNING id`
 
 	// 쿼리 실행
-	err := repo.DB.QueryRow(query, user.ID, user.Nickname, user.State, user.UpdatedAt, user.UpdatedBy).Scan(&user.ID)
+	err := repo.DB.QueryRow(query, user.ID, user.Nickname, user.State, user.UpdatedAt, user.Nickname).Scan(&user.ID)
 	if err != nil {
 		log.Printf("Error updating user: %v", err)
 		return nil, fmt.Errorf("error updating user: %w", err)
@@ -60,11 +60,11 @@ func (repo *UserRepositoryImpl) DeleteUser(userID int) error {
 }
 
 func (repo *UserRepositoryImpl) GetUserByID(userID int) (*model.User, error) {
-	query := `SELECT id, nickname, state, created_at, created_by, updated_at, updated_by FROM user WHERE id = $1`
+	query := `SELECT id, nickname, state, created_at, updated_at FROM user WHERE id = $1`
 
 	// 쿼리 실행
 	user := model.User{}
-	err := repo.DB.QueryRow(query, userID).Scan(&user.ID, &user.Nickname, &user.State, &user.CreatedAt, &user.CreatedBy, &user.UpdatedAt, &user.UpdatedBy)
+	err := repo.DB.QueryRow(query, userID).Scan(&user.ID, &user.Nickname, &user.State, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			log.Printf("No user found for ID: %v", userID)
