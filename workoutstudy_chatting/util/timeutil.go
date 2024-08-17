3161ab3c84e83d8e209075eba1e4c1fd1dcea6e6
Kimsunfang <kimsunfang@gmail.com>
2024-08-18 00:28:43 +0900

    FIX : must fix url

--- workoutstudy_chatting/util/timeutil.go
+++ workoutstudy_chatting/util/timeutil.go
@@ -2,19 +2,12 @@ package util
 
 import (
 	"log"
-	"strings"
 	"time"
 )
 
 // ParseMessageTime 함수는 시간 문자열을 입력받아 time.Time 객체를 반환합니다.
 func ParseMessageTime(timeStr string) (time.Time, error) {
-	// 만약 타임존에 + 또는 - 기호가 없다면 +를 추가해줍니다.
-	if len(timeStr) > 19 && strings.Contains(timeStr, "") && !strings.Contains(timeStr, "+") && !strings.Contains(timeStr, "-") {
-		// 공백 이후 타임존 부분을 가져와서 +를 추가
-		timeStr = strings.TrimSpace(timeStr[:len(timeStr)-6]) + "+09:00"
-	}
-
-	// 커스텀 레이아웃 정의: 타임존 정보가 있는 경우를 처리합니다.
+	// 커스텀 레이아웃: 타임존이 있는 경우를 처리합니다.
 	const customLayout = "2006-01-02 15:04:05.999999-07:00"
 
 	// 커스텀 레이아웃을 사용하여 시간 문자열을 파싱합니다.
