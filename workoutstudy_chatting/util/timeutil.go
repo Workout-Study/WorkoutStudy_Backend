1390c09dd252b5e5e7f1d4dfc075fa7320da4287
Kimsunfang <kimsunfang@gmail.com>
2024-08-18 00:33:32 +0900

    FIX : add message time log

--- workoutstudy_chatting/util/timeutil.go
+++ workoutstudy_chatting/util/timeutil.go
@@ -7,7 +7,10 @@ import (
 
 // ParseMessageTime 함수는 시간 문자열을 입력받아 time.Time 객체를 반환합니다.
 func ParseMessageTime(timeStr string) (time.Time, error) {
-	// 커스텀 레이아웃: 타임존이 있는 경우를 처리합니다.
+	// 입력된 timeStr 로그로 출력
+	log.Printf("Original timeStr in ParseMessageTime: %s", timeStr)
+
+	// 커스텀 레이아웃 정의: 타임존 정보가 있는 경우를 처리합니다.
 	const customLayout = "2006-01-02 15:04:05.999999-07:00"
 
 	// 커스텀 레이아웃을 사용하여 시간 문자열을 파싱합니다.
@@ -17,6 +20,8 @@ func ParseMessageTime(timeStr string) (time.Time, error) {
 		return time.Time{}, err
 	}
 
-	log.Printf("파싱된 시간: %s", parsedTime)
+	// 파싱된 시간 로그로 출력
+	log.Printf("Parsed time in ParseMessageTime: %s", parsedTime)
+
 	return parsedTime, nil
 }
